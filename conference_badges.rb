ec7527a6ae9aaaee63feedc68eaa9535a1b7bbca
Crystal Norman <cnorm88@gmail.com>
2020-09-16 16:18:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def
-
-# def batch_badge_creator(people)
-#   people.map do |person|
-    
-#   end
+def batch_badge_creator(people)
+  people.map do |person|
+end  
